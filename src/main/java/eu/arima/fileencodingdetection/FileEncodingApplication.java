bbdef6e2af26909aa3c3904a0be12395ead07512
Iñigo Telleria <inigo@arima.eu>
2018-04-25 18:27:26 +0200

    Redirect / to /fileencoding

--- src/main/java/eu/arima/fileencodingdetection/FileEncodingApplication.java
+++ src/main/java/eu/arima/fileencodingdetection/FileEncodingApplication.java
@@ -2,11 +2,31 @@ package eu.arima.fileencodingdetection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class FileEncodingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FileEncodingApplication.class, args);
-	}
+  public static void main(final String[] args) {
+    SpringApplication.run(FileEncodingApplication.class, args);
+  }
+
+  @Configuration
+  public static class MyWebMvcConfig {
+
+    @Bean
+    public WebMvcConfigurer forwardToIndex() {
+      return new WebMvcConfigurer() {
+        @Override
+        public void addViewControllers(final ViewControllerRegistry registry) {
+          registry.addRedirectViewController("/", "/fileencoding");
+        }
+      };
+    }
+
+  }
 }
